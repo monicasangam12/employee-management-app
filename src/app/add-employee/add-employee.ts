14d36d0373e4b5911bc3d3f5fb34f8fcc4fd2f31
monicasangam12 <47582209+monicasangam12@users.noreply.github.com>
2025-09-18 13:24:43 -0400

    Update add-employee.ts

--- src/app/add-employee/add-employee.ts
+++ src/app/add-employee/add-employee.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { Employee } from '../../employee';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './add-employee.html',
   styleUrl: './add-employee.css'
 })
 export class AddEmployeeComponent {
+
+  employeeForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.employeeForm = this.fb.group({
+      name: [''],
+      username: [''],
+      password: [''],
+      position: [''],
+      salary: [''],
+      rating: ['']
+    });
+    console.log(this.employeeForm);
+  }
 
   addEmployee(){ 
     const employee = new Employee(0, '', '', 0, '', '', 0);
@@ -16,3 +32,4 @@ export class AddEmployeeComponent {
   }
 
 }
+
